708bf0ab2c2613fab9c820344775eb8e65972c6c
Saxo Cat <92432250+SaxoCatto@users.noreply.github.com>
2024-09-20 00:06:52 +0700

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// add extra headers that i make here. will bits/stdc++ work here?
+
+int main(int argc, char* argv[]) {
+    if (handle error) {
+      printf("No database filename given!\n");
+      exit(EXIT_FAILURE);
+    }
+
+    // need input buffer. so we make and call it
+
+    // actual fuction is encased in a while(true) loop. it helps us with things like prompting user after an action.
+
+    // ofc specific error like unknown command must be considered.
+}
